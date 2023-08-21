cfc72904d62de3b0dd04e0703fd587b732b2c115
Nikolay Magarin <magarinnikolay@gmail..com>
2023-08-21 20:08:41 +0300

    Добавлено тестовое бинарное дерево

---
+++ data/binaryTree.js
@@ -0,0 +1,31 @@
+const tree = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+    },
+    right: {
+      val: 5,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+    },
+    right: {
+      val: 7,
+    },
+  },
+};
+
+/*
+
+      1
+    /   \
+  2       3
+ / \     / \
+4   5   6   7
+
+*/
